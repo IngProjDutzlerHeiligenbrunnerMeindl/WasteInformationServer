326d1decae09c677e1f5123d03c65c57c997386c
Lukas-Heiligenbrunner <Lukas.Heiligenbrunner@gmail.com>
2019-09-13 15:57:33 +0200

    added jquery and js to project

--- src/main.java
+++ src/main.java
@@ -1,15 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        Thread mythread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new Webserver().startserver();
-            }
-        });
+        Thread mythread = new Thread(() -> new Webserver().startserver());
         mythread.start();
         System.out.println("thread started");
-
     }
-
-
 }

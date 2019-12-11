97214d786e1e0032137453a7d297e03dc1e098de
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2019-12-11 13:17:55 +0100

    * generate build time property in gradle file

--- src/java/com/wasteinformationserver/basicutils/Info.java
+++ src/java/com/wasteinformationserver/basicutils/Info.java
@@ -1,11 +1,8 @@
 package com.wasteinformationserver.basicutils;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
 
 public class Info {
@@ -16,20 +13,13 @@ public class Info {
     public static void init(){
         Properties prop = new Properties();
         try {
-            SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-
             URL url = Info.class.getResource("/version.properties");
-
-            String builddatee = format.format(new Date(new File(url.toURI()).lastModified()));
-            builddate=builddatee;
 
             prop.load(url.openStream());
             version=(String)prop.get("version");
+            builddate=(String)prop.get("buildtime");
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
-
     }
 }

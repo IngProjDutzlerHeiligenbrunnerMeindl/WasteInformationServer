7e8a857caf54c1413d0ca0fa0bdf15e0b26a2838
Lukas-Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2020-02-10 11:16:44 +0100

    define class for AdminPanel

--- src/resources/wwwroot/js/adminpanel.js
+++ src/resources/wwwroot/js/adminpanel.js
@@ -1,34 +1,45 @@
 $(document).ready(function () {
-    console.log("page loaded");
-    $.post('/senddata/checkloginstate', 'action=getloginstate', function (data) {
-        console.log(data);
-        if (data.loggedin == true) {
-            $("#userlabel").html(" " + data.username);
-            if (data.permission > 0) {
-                $("#adminpanel").show();
-            }
-        } else {
-            $("#userlabel").html(" not logged in!!");
-        }
-    }, 'json');
-
-
-    $("#btn-shutdown").click(function (event) {
-        console.log("shutting down server");
-
-        $.post('/senddata/admindata', 'action=shutdownserver', function (data) {
-            console.log(data);
-
-        }, 'json');
-
-    });
-
-    $("#btn-restart").click(function (event) {
-        console.log("restarting server");
-
-        $.post('/senddata/admindata', 'action=restartserver', function (data) {
-            console.log(data);
-
-        }, 'json');
-    });
+    new AdminPanel();
 });
+
+class AdminPanel {
+    constructor() {
+        this.checkLoginState();
+        this.addClickListeners();
+    }
+
+    checkLoginState(){
+        $.post('/senddata/checkloginstate', 'action=getloginstate', function (data) {
+            console.log(data);
+            if (data.loggedin == true) {
+                $("#userlabel").html(" " + data.username);
+                if (data.permission > 0) {
+                    $("#adminpanel").show();
+                }
+            } else {
+                $("#userlabel").html(" not logged in!!");
+            }
+        }, 'json');
+    }
+
+    addClickListeners(){
+        $("#btn-shutdown").click(function (event) {
+            console.log("shutting down server");
+
+            $.post('/senddata/admindata', 'action=shutdownserver', function (data) {
+                console.log(data);
+
+            }, 'json');
+
+        });
+
+        $("#btn-restart").click(function (event) {
+            console.log("restarting server");
+
+            $.post('/senddata/admindata', 'action=restartserver', function (data) {
+                console.log(data);
+
+            }, 'json');
+        });
+    }
+}

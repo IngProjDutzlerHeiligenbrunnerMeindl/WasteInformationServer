0498e61616107028466036d1a92756eeab48ec32
lukas-heiligenbrunner <lheilige@htl-steyr.ac.at>
2020-03-27 17:53:33 +0100

    delete unused profile and demo files

--- src/resources/wwwroot/js/profile.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#firstname").value("hhh");
-
-    $.post('/senddata/checkloginstate', 'action=getfirstname', function (data) {
-        console.log(data);
-    }, 'json');
-});
